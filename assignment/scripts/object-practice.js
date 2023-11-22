6adcd2d60a85caab34d312fa8e85e87113ef9f96
sseang <142060734+sseang@users.noreply.github.com>
2023-11-21 22:13:24 -0600

    Completed question 3

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -1,33 +1,43 @@
-console.log('***** Object Practice *****')
+console.log("***** Object Practice *****");
 
 // Add the required code to complete the tasks below
 
 /* 1. Create an object literal to represent you!
-  - Give the object a 'firstName & (separate) 'lastName' property
-  - Give it a boolean 'hasSiblings' property
-  - Give it  a numeric 'shoeCount' property
-  - Also give it an array 'favThreeFoods'
+[x]  - Give the object a 'firstName & (separate) 'lastName' property
+[x]  - Give it a boolean 'hasSiblings' property
+[x]  - Give it  a numeric 'shoeCount' property
+[x]  - Also give it an array 'favThreeFoods'
 */
+
 const me = {
   // TODO - add properties here
+
+  firstName: 'Sarith',
+  lastName: 'Seang',
+  hasSiblings: true, 
+  shoeCount: 20,
+  faveThreeFoods: ['Miyazaki Beef', 'KFC', 'Burgers'],
 };
-console.log('A little about me:', me);
+
+console.log("A little about me:", me);
 
 /* 2. Accessing object properties.
-  - Create a variable called fullName 
-  - Use the firstName & lastName properties of the object you 
-    created above to set its value. 
-  - Console.log fullName
+[x]  - Create a variable called fullName 
+[x]  - Use the firstName & lastName properties of the object you 
+[x]    created above to set its value. 
+[x]  - Console.log fullName
 */
 
-
+const fullName = `${me.firstName} ${me.lastName}`;
+console.log(fullName);
 
 /* 3. Nested arrays
-  - Console.log your first favorite food
-  - Console.log your last favorite food 
+[x]  - Console.log your first favorite food
+[x]  - Console.log your last favorite food 
 */
 
-
+console.log(me.faveThreeFoods[0]);
+console.log(me.faveThreeFoods[2]);
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -35,7 +45,6 @@ console.log('A little about me:', me);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
